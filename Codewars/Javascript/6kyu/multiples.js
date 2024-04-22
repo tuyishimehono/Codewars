ef65e1e2e4b5f66b58511032f1166b18fa314750
Honorine <tuhonori1@gmail.com>
2024-04-22 10:34:36 +0200

    Added main files

--- Codewars/Javascript/6kyu/multiples.js
+++ Codewars/Javascript/6kyu/multiples.js
@@ -4,14 +4,14 @@
     return the sum
 */
 
-function solution(number){
-    var sum = 0;
-    
-    for(var i = 1;i< number; i++){
-      if(i % 3 == 0 || i % 5 == 0){
-        sum += i
-      }
+function solution(number) {
+  var sum = 0;
+
+  for (var i = 1; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
     }
-    return sum;
+  }
+  return sum;
 }
 console.log(solution(10));
