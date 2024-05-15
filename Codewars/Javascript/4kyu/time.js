dde2ccde1a91091908b1bbf6678be11b8ab0155e
tuyishimehono <tuhonori1@gmail.com>
2024-05-15 23:25:34 +0200

    updated time.js

--- Codewars/Javascript/4kyu/time.js
+++ Codewars/Javascript/4kyu/time.js
@@ -2,14 +2,19 @@ function time(seconds) {
     seconds = Number(seconds);
     seconds = Math.abs(seconds);
     
-    const y = Math.florr(seconds/(3600 * 24 * 365))
-    const d = Math.floor(seconds % ((3600 * 24 * 365)));
-    (sec % (60 * 60 * 24 * 365)) / (60 * 24)
+    const y = Math.floor(seconds/(3600 * 24 * 365))
+    const d = Math.floor(seconds % ((3600 * 24 * 365))/ (3600*365));
+    //(sec % (60 * 60 * 24 * 365)) / (60 * 24)
     const h = Math.floor((seconds % (3600 * 24)) / 3600);
     const m = Math.floor((seconds % 3600) / 60);
     const s = Math.floor(seconds % 60);
   
     const parts = [];
+
+    if(y>0) {
+        let yDisplay = d > 0 ? d + ' ' + (y == 1 ? "year" : "years") : "";
+        parts.push(yDisplay);
+    }
   
     if (d > 0) {
         let dDisplay = d > 0? d + ' ' + (d == 1? "day" : "days") : "";
@@ -30,9 +35,11 @@ function time(seconds) {
         let sDisplay = s > 0? s + ' ' + (s == 1? "second" : "seconds") : "";
         parts.push(sDisplay);
     }
-  
+    
+    if(parts.length > 1){
+        parts[parts.length-1] = 'and ' + parts[parts.length -1]; 
+    }
     return parts.join(', ');
   }
   
-  
-  console.log(time(205851834));
+console.log(time(205851834));
