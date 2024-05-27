a5b96d909730aace5769b8621a1b46738615512e
tuyishimehono <tuhonori1@gmail.com>
2024-05-27 17:19:42 +0200

    Updated reversedObject.js

--- Codewars/Javascript/ES6/reversedObject.js
+++ Codewars/Javascript/ES6/reversedObject.js
@@ -1,11 +1,10 @@
 // Write a function that takes an object as input and returns a new object with all the key-value pairs reversed (keys become values and values become keys)
 // const originalObj = {aa: "1", bf: "3", cq: "5"};
 // const reversedObj = reverseObject(originalObj);
-// // console.log(reversedObj); 
+// console.log(reversedObj); 
 // output: {"1": 'aa', "3": 'bf', "5": 'cq'} // Expected output
 
 function reverseObject(obj){
-
     const reversedEntries = Object.entries(obj).map(([key, value]) => [value, key]);
     return Object.fromEntries(reversedEntries);
 }
