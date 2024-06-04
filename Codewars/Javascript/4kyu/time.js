9f283e9b80e7d7cd995cd485b1fc34cfff50bd7c
tuyishimehono <tuhonori1@gmail.com>
2024-06-04 14:59:17 +0200

    fix: correct year algorithm

--- Codewars/Javascript/4kyu/time.js
+++ Codewars/Javascript/4kyu/time.js
@@ -3,7 +3,7 @@ function time(seconds) {
   seconds = Math.abs(seconds);
 
   const y = Math.floor(seconds / (3600 * 24 * 365));
-  const d = Math.floor((seconds % (3600 * 24 * 365)) / (3600 * 25));
+  const d = Math.floor((seconds % (3600 * 24 * 365)) / (3600 * 24));
   const h = Math.floor((seconds % (3600 * 24)) / 3600);
   const m = Math.floor((seconds % 3600) / 60);
   const s = Math.floor(seconds % 60);
