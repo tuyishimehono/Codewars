279d03b11f8e88e2ddd1ee80568009e6a3082f2a
tuyishimehono <tuhonori1@gmail.com>
2024-06-05 18:33:54 +0200

    add stringPairing.js

---
+++ Codewars/Javascript/6kyu/stringPairing.js
@@ -0,0 +1,12 @@
+function solution(str){
+    if(str.length %2 != 0){
+        str += '_'
+    }
+    if(str === ''){
+      return []
+    }
+    return str.match(/.{1,2}/g)
+  }
+  console.log(solution(''));
+  console.log(solution('abcd'));
+  console.log(solution('abcdefg'));
