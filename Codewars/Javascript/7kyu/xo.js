3c46fdfbd4064fd4a2b1258943d4eabc72b23fca
tuyishimehono <tuhonori1@gmail.com>
2024-06-10 17:47:25 +0200

    add xo.js

---
+++ Codewars/Javascript/7kyu/xo.js
@@ -0,0 +1,10 @@
+function XO(str) {
+  let arr = str.toLowerCase().split("");
+  let count1 = 0;
+  let count2 = 0;
+  arr.map((e) => {
+    if (e === "o") count1 += 1;
+    if (e === "x") count2 += 1;
+  });
+  return count1 === count2;
+}
