dacdb9f110bb9b712412a86c57cb4a72d13e7de8
tuyishimehono <tuhonori1@gmail.com>
2024-06-12 22:25:51 +0200

    restyle deepEqual.js

--- Codewars/Javascript/ES6/deepEqual.js
+++ Codewars/Javascript/ES6/deepEqual.js
@@ -1,58 +1,29 @@
-// Create a function that performs a deep comparison between two arrays of objects, checking for equality of nested properties.
-
-// const array1 = [
-//     { id: 1, name: "Alice", details: { age: 30, city: "New York" } },
-//     { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } }
-// ];
-
-// const array2 = [
-//     { id: 1, name: "Alice", details: { age: 30, city: "New York" } },
-//     { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } }
-// ];
-
-// // console.log(deepEqualArrays(array1, array2));
-// output: true
-// ......................................................................................
-
-// const array1 = [
-//     { id: 1, name: "Alice", details: { age: 30, city: "New York" } },
-//     { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } }
-// ];
-
-// const array2 = [
-//     { id: 3, name: "Alice ", details: { age: 30, city: "New York" } },
-//     { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } }
-// ];
-
-// // console.log(deepEqualArrays(array1, array2));
-// output: false
-
-function deepEqualArrays(arr1, arr2){
-    const equality = JSON.stringify(arr1) === JSON.stringify(arr2);
-    return equality;
+function deepEqualArrays(arr1, arr2) {
+  const equality = JSON.stringify(arr1) === JSON.stringify(arr2);
+  return equality;
 }
 
 const array1 = [
-    { id: 1, name: "Alice", details: { age: 30, city: "New York" } },
-    { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } }
+  { id: 1, name: "Alice", details: { age: 30, city: "New York" } },
+  { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } },
 ];
 
 const array2 = [
-    { id: 3, name: "Alice ", details: { age: 30, city: "New York" } },
-    { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } }
+  { id: 3, name: "Alice ", details: { age: 30, city: "New York" } },
+  { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } },
 ];
 
 console.log(deepEqualArrays(array1, array2));
 //output: false
 
 const array3 = [
-    { id: 1, name: "Alice", details: { age: 30, city: "New York" } },
-    { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } }
+  { id: 1, name: "Alice", details: { age: 30, city: "New York" } },
+  { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } },
 ];
 
 const array4 = [
-    { id: 1, name: "Alice", details: { age: 30, city: "New York" } },
-    { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } }
+  { id: 1, name: "Alice", details: { age: 30, city: "New York" } },
+  { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } },
 ];
 
 console.log(deepEqualArrays(array3, array4));
