2e73be0d62bfe5c1fea47c7a45036f6af366831b
tuyishimehono <tuhonori1@gmail.com>
2024-06-28 19:22:07 +0200

    refactor duplicateCount.js

--- Codewars/Javascript/6kyu/duplicateCount.js
+++ Codewars/Javascript/6kyu/duplicateCount.js
@@ -1,12 +1,10 @@
-function duplicateCount(text){
+const duplicateCount = (text) => {
     let duplicate = 0;
-     const counter = {}
-     text.toLowerCase().split('').forEach(e=> counter[e] ? counter[e]+=1 : counter[e] = 1 )
-     for(let i in counter){
-         if(counter[i] > 1){
-             duplicate++
-         }
-     }
+    const counter = {}
+    text.toLowerCase().split('').forEach(e=> counter[e] ? counter[e]+=1 : counter[e] = 1 )
+    for(let i in counter){
+         if(counter[i] > 1) duplicate++
+    }
      return duplicate
 }
 console.log(duplicateCount("abcde"), 0);
