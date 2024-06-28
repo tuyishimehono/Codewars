0714221b6ec0737ae387a5abba5e371a57635dfa
tuyishimehono <tuhonori1@gmail.com>
2024-06-28 16:50:35 +0200

    adjust onlyDuplicates.js

--- Codewars/Javascript/6kyu/onlyDuplicates.js
+++ Codewars/Javascript/6kyu/onlyDuplicates.js
@@ -1,11 +1,15 @@
-
-function onlyDuplicates(str) {
+const onlyDuplicates = (str) => {
     const charCounts = {};
     for (let char of str) {
         charCounts[char] = (charCounts[char] || 0) + 1;
     }
     return str.split('').filter(char => charCounts[char] > 1).join('');
 }
+//Best practice solution
+// const onlyDuplicates = () => {
+//     return str.split('').filter(e => str.indexOf(e) !== str.lastIndexOf(e)).join('');
+// }
+
 console.log(onlyDuplicates('abccdefee'), 'cceee')
 console.log(onlyDuplicates('hello'), 'll')
 console.log(onlyDuplicates('colloquial'), 'ollol')
