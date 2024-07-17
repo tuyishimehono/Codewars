213d5482063bde967897449c23a21b3f276f0558
tuyishimehono <tuhonori1@gmail.com>
2024-07-17 14:52:32 +0200

    refactor ending.ts

--- Codewars/TypeScript/7kyu/ending.ts
+++ Codewars/TypeScript/7kyu/ending.ts
@@ -2,6 +2,7 @@ function solution(str: string, ending: string): boolean {
     if(ending === '') return true;
     return (str.slice(-(ending.length)) === ending) ? true : false;
 }
+console.log(solution('abcde', 'cde')); // true
 
 //best practice solution
 function solution2(str: string, ending: string): boolean {
