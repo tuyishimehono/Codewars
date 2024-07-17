d429722cca4f4bdcc15200a0760f13d9f55bbd4e
tuyishimehono <tuhonori1@gmail.com>
2024-07-17 14:52:00 +0200

    refactor ending.ts

--- Codewars/TypeScript/7kyu/ending.ts
+++ Codewars/TypeScript/7kyu/ending.ts
@@ -2,3 +2,8 @@ function solution(str: string, ending: string): boolean {
     if(ending === '') return true;
     return (str.slice(-(ending.length)) === ending) ? true : false;
 }
+
+//best practice solution
+function solution2(str: string, ending: string): boolean {
+    return str.endsWith(ending);
+}
