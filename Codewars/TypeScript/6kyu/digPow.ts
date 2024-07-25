7d023a3026b0ae6bbb9fc6b5875a574d3512d8a6
tuyishimehono <tuhonori1@gmail.com>
2024-07-25 20:15:56 +0200

    Edit digPow.ts

--- Codewars/TypeScript/6kyu/digPow.ts
+++ Codewars/TypeScript/6kyu/digPow.ts
@@ -1,1 +1,9 @@
-const digPow()
+const digPow = (n: number, p: number) => {
+    const arr = Array.from(String(n), Number)
+    let sum = 0;
+    arr.map(e => {
+      sum += e**p
+      p+=1
+    })
+    return sum%n === 0 ? sum/n : -1
+}
