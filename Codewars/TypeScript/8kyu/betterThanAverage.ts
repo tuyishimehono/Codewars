5fe4a5788a8f4076bf08f6536f9f4165b3626aac
tuyishimehono <tuhonori1@gmail.com>
2024-07-29 19:33:24 +0200

    Add test cases for betterThanAverage.ts

--- Codewars/TypeScript/8kyu/betterThanAverage.ts
+++ Codewars/TypeScript/8kyu/betterThanAverage.ts
@@ -2,3 +2,5 @@ function betterThanAverage(classPoints: number[], yourPoints: number) : boolean 
     const sum = classPoints.reduce((acc,current) => acc + current )
     return sum/classPoints.length < yourPoints;
 }
+console.log(betterThanAverage([45,21,67,88,42,30], 70))
+console.log(betterThanAverage([83,46,98], 40))
