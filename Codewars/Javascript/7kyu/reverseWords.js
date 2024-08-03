377fd88ae3e71c51d25c075594d6ab2b34eb6905
tuyishimehono <tuhonori1@gmail.com>
2024-08-03 23:58:15 +0200

    Add test cases for reverseWords.js

--- Codewars/Javascript/7kyu/reverseWords.js
+++ Codewars/Javascript/7kyu/reverseWords.js
@@ -1,3 +1,5 @@
 function reverseWords(str) {
     return str.split(" ").map(e=>e.split("").reverse().join("")).join(" ")
 }
+reverseWords("This is an example!") // output: "sihT si na !elpmaxe"
+reverseWords("double  spaces") // ==> "elbuod  secaps"
