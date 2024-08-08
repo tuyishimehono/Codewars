b610878c64a939af353cd54443bde2d3a1d04a4e
tuyishimehono <tuhonori1@gmail.com>
2024-08-08 14:24:45 +0200

    Refactor algorithm for persistence.ts

--- Codewars/TypeScript/6kyu/persistence.ts
+++ Codewars/TypeScript/6kyu/persistence.ts
@@ -1,10 +1,11 @@
 function persistence(num: number): number {
-    let numbers = num.toString().split('').map(Number)
     let count = 0;
-    while(numbers.length > 1) {
-      let mult = numbers.reduce((acc,curr)=> acc * curr)
+    while(num > 9) {
+      num = num.toString().split('').map(Number).reduce((acc,curr)=> acc * curr)
       count ++ 
-      numbers = mult.toString().split('').map(Number)
     }
     return count
 }
+//test cases
+persistence(456) // 2
+persistence(39) // 2
