8709eedad24f81f30a6656bda6f7120c850f736f
tuyishimehono <tuhonori1@gmail.com>
2024-08-28 23:32:19 +0200

    Add roman encoder function

---
+++ Codewars/Javascript/6kyu/romanEncoder.js
@@ -0,0 +1,26 @@
+function solution(number) {
+  const romanCodes = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1],
+  ];
+  let result = "";
+  let num = number;
+  romanCodes.forEach(([symbol, value]) => {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  });
+  return result;
+}
